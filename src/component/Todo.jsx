56bb2ca3124db6e9b18bef6cb16556de5a41f62f
Kadir Deniz <kadir.deniz@ekos.ai>
2022-11-26 23:36:47 +0300

    todo form

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import TodoForm from "./TodoForm";
 
 const Todo = () => {
-  return <div className="todo" id="todo"></div>;
+  return <div className="todo" id="todo">
+    <TodoForm/>
+  </div>;
 };
 
 export default Todo;
---
+++ src/component/TodoForm.jsx
@@ -0,0 +1,5 @@
+const TodoForm = ()=>{
+    return <h1>Todo Form</h1>
+}
+
+export default TodoForm
