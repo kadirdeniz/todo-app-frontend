3b7e707d9ee5ddb5ae084f4b8561ed2aeed42d07
Kadir Deniz <kadir.deniz@ekos.ai>
2022-11-26 22:51:09 +0300

    succeed first component test

--- src/__tests__/component/App.spec.js
+++ src/__tests__/component/App.spec.js
@@ -2,6 +2,11 @@ import { shallow } from "enzyme";
 import App from "../../App";
 import Todo from "../../component/Todo";
 
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
 const wrapper = shallow(<App />);
 
 // App component will have only Todo component
