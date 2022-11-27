83c1e9e923ad72fda182b587a8b13c25a903b017
Kadir Deniz <kadir.deniz@ekos.ai>
2022-11-27 21:51:39 +0300

    dockerfile

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // send post request to todo server
 export const createTodo = async (baseUrl, todo) => {
-  const response = await axios.post(`${baseUrl}/todos`, {
+  const response = await axios.post(`${baseUrl}/todo`, {
     text: todo,
   });
   return response.data;
